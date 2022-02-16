3fdfe2b08da8410f546c8bb15a0845ff2530a4a0
kelly0924 <hyj09240@gmail.com>
2022-02-16 13:35:59 +0000

    update dragGame

--- dragDrop.js
+++ dragDrop.js
@@ -1,22 +1,69 @@
 
-var tmpTime=document.getElementById("mainDivTime");
-var index=0;
+// 강아지 이미지를 랜덤으로 보여 주겠다
+var dogImg=[];
+var setDogImg;
+var tmpDropImg;
+var dragIndex;
+var resultImg = [];
+var ranIndex=[2,1,5,6,10,9,3,11,4,7,0,8];
 window.onload=function(){
-    for(var i=0; i< 5; i++){//5번 반복하면서 타이머 이미지 보여주기 
-        var doingTime=setTimeout(countTime(),1000);
-
+    var tmpImg=document.getElementsByClassName("mainDivImgDiv");
+    var tmpDiv=document.getElementsByClassName("mainDivImgDiv");//div에 다시 넣어 주기 위해
+    for(var index = 0; index < tmpImg.length; index++){
+       dogImg[ranIndex[index]]=tmpImg[index].innerHTML;
+       resultImg[index]=tmpImg[index].innerHTML;
     }
-    tmpTime.style.display="none";
-    document.getElementById("mainDivSelect").style.display="block";
-    document.getElementById("mainDivMake").style.display="block";
-
+    for(var index = 0; index < tmpImg.length; index++){
+        tmpDiv[index].innerHTML=dogImg[index];
+    }
+    console.log(dogImg);
+    console.log(ranIndex.length);
 }
-function countTime(){
-  
-  for(var i = 0; i < 5; i++){
-    var tmpTimeImg=document.getElementsByTagName("mainDivtimeImg");
-    //window.getComputedStyle(tmpTimeImg[i]).display="block";
-    tmpTime.innerHTML=tmpTimeImg[i];
-  }
 
+function getImgDragEvent(index){//드래그 시작 하면서 실행 되는 이벤트 
+    //index 는 0부터 11까지
+    setDogImg=document.getElementsByClassName("mainDivImgDiv")[index].innerHTML
+    // console.log(index);
+    // console.log("setDog"+setDogImg);
+    dragIndex = index;
+    
+}
+
+function setImgOverEvent(){//드래그 이벤트가 되는 동안 실행 되는 이벤트
+    event.preventDefault()
+}
+
+function setImgDropEvent(index){
+    tmpDropImg = document.getElementsByClassName("mainDivImgDiv")[index].innerHTML;//드롭 될 곳에 이미지 기억 하기 
+    //document.getElementsByClassName("mainDivImgDiv")[index].innerHTML="";
+    document.getElementsByClassName("mainDivImgDiv")[index].innerHTML=setDogImg;
+    //document.getElementsByClassName("mainDivImgDiv")[dragIndex].innerHTML=" ";
+    document.getElementsByClassName("mainDivImgDiv")[dragIndex].innerHTML=tmpDropImg;
+    // console.log(index);
+    // console.log(document.getElementsByClassName("mainDivImgDiv")[index].innerHTML=setDogImg);
+   
+}
+
+function compareImg(){
+   var nowImgArr=[];
+   var cnt=true;
+   var nowImg= document.getElementsByClassName("mainDivImgDiv");
+   console.log(resultImg)
+   for(var index = 0; index < resultImg.length; index++){
+        nowImgArr[index]=nowImg[index].innerHTML;
+    }
+    for(var i = 0; i<resultImg.length; i++){
+        if(nowImgArr[i] == resultImg[i]){
+            continue;
+        }else{
+            alert("다시 하기");
+            cnt=false;
+            break;
+        }
+    }
+    if(cnt == true){
+        alert("성공성공");
+    }
+    console.log(nowImgArr)
+    
 }
